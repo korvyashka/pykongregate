624be6398d3e89872e664e4ebd4bfc4239e3a352
korvyashka <korvyashka666@gmail.com>
2015-05-04 16:03:06 +0300

    add get_items, refactor a bit

--- pykongregate/api.py
+++ pykongregate/api.py
@@ -2,10 +2,11 @@ import requests
 import simplejson
 
 
-GET_ITEMS_URL = "http://www.kongregate.com/api/user_items.json"
+GET_USER_ITEMS_URL = "http://www.kongregate.com/api/user_items.json"
+GET_ITEMS_URL = "http://www.kongregate.com/api/items.json"
 
 
-def get_items_api(user_id, api_key):
+def get_user_items_api(user_id, api_key):
     """
     wrapper on API method:
     http://developers.kongregate.com/docs/rest/user-items
@@ -21,6 +22,26 @@ def get_items_api(user_id, api_key):
     params = {
         'api_key': api_key,
         'user_id': user_id,
+    }
+    response = requests.get(
+        GET_USER_ITEMS_URL, params=params
+    )
+    return simplejson.loads(response.text)
+
+
+def get_items_api(api_key):
+    """
+    wrapper on API method:
+    http://developers.kongregate.com/docs/rest/items
+
+    :param api_key:
+    :type api_key: str
+
+    :rtype: dict
+    :return: loaded json from response
+    """
+    params = {
+        'api_key': api_key,
     }
     response = requests.get(
         GET_ITEMS_URL, params=params
--- pykongregate/models.py
+++ pykongregate/models.py
@@ -1,4 +1,4 @@
-from .api import get_items_api
+from .api import get_items_api, get_user_items_api
 from .exceptions import ApiException
 
 
@@ -12,19 +12,36 @@ class KongApi(object):
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def get_items(self, user):
+    def get_user_items(self, user):
         """
         get items for user
 
         :param user:
         :type user: KongUser
 
+        :rtype: list of KongUserItem
+        :return: items list
+        """
+        response = self.api_call(
+            get_user_items_api,
+            user.user_id, self.api_key
+        )
+        new_items = map(
+            lambda x: KongUserItem(**x),
+            response['items']
+        )
+        return new_items
+
+    def get_items(self):
+        """
+        get available items
+
         :rtype: list of KongItem
         :return: items list
         """
         response = self.api_call(
             get_items_api,
-            user.user_id, self.api_key
+            self.api_key
         )
         new_items = map(
             lambda x: KongItem(**x),
@@ -90,12 +107,23 @@ class KongUser(object):
         """
         if self._items and not refetch:
             return self._items
-        new_items = self.api.get_items(self)
+        new_items = self.api.get_user_items(self)
         self._items = new_items
         return new_items
 
     def has_item(self, id=None, identifier=None):
+        """
+        Determines if user has item with criteria.
 
+        :param id: unique transaction_id of kongregate.
+        :type id: int
+        :param identifier: bank_item name. User can buy several of them.
+            Return True if at least one is there.
+        :type identifier: str
+
+        :rtype: bool
+        :return:
+        """
         def _id_filter(x):
             return x.id == id
 
@@ -104,17 +132,45 @@ class KongUser(object):
 
         if id is None and identifier is None:
             return ValueError("Provide id or identifier")
-        if id is not None:
-            filter_key = _id_filter
-        else:
-            # identifer is not None
-            filter_key = _identifier_filter
 
-        items = filter(filter_key, self.items(refetch=False))
+        items = self.items()
+        if id is not None:
+            items = filter(_id_filter, items)
+        if identifier is not None:
+            items = filter(_identifier_filter, items)
+
         return bool(items)
 
+    def __repr__(self):
+        return "KongUser: {}".format(self.user_id)
 
-class KongItem(object):
+
+class KongBaseItem(object):
+    def __init__(
+        self,
+        id=None,
+        identifier=None,
+        name=None,
+        description=None,
+        **kwargs
+    ):
+        self.id = id
+        self.identifier = identifier
+        self.name = name
+        self.description = description
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __repr__(self):
+        return "{}: [id:{}, identifier: {}]".format(
+            self.__class__.__name__,
+            self.id,
+            self.identifier
+        )
+
+
+class KongUserItem(KongBaseItem):
     """
     Kongregate user item
 
@@ -132,14 +188,59 @@ class KongItem(object):
     :type data:
     """
     def __init__(
-        self, id, identifier,
-        name, description, remaining_uses,
-        data
+        self,
+        id=None,
+        identifier=None,
+        name=None,
+        description=None,
+        remaining_uses=None,
+        data=None,
+        **kwargs
     ):
-        self.id = id
-        self.identifier = identifier
-        self.name = name
-        self.description = description
+        super(KongUserItem, self).__init__(
+            id=id,
+            identifier=identifier,
+            name=name,
+            description=description,
+            **kwargs
+        )
         self.remaining_uses = remaining_uses
         self.data = data
 
+
+class KongItem(KongBaseItem):
+    """
+    Kongregate user item
+
+    :param id:
+    :type id:
+    :param identifier:
+    :type identifier:
+    :param name:
+    :type name:
+    :param description:
+    :type description:
+    :param price:
+    :type price:
+    :param tags:
+    :type tags:
+    """
+    def __init__(
+        self,
+        id=None,
+        identifier=None,
+        name=None,
+        description=None,
+        price=None,
+        tags=None,
+        **kwargs
+    ):
+        super(KongItem, self).__init__(
+            id=id,
+            identifier=identifier,
+            name=name,
+            description=description,
+            **kwargs
+        )
+        self.price = price
+        self.tags = tags
